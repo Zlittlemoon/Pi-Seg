3b3e222225befe7297833ecb17b0bf8c77d3dcf5
Bingyu Li USTC <124509769+LiBingyu01@users.noreply.github.com>
2026-03-28 14:58:48 +0800

    Delete KEY.sh

--- KEY.sh
+++
@@ -1,7 +0,0 @@
-
-# ######################################################################################
-sh run.sh configs/vitl_336_DLRSD.yaml 4 output_vitl_336_DLRSD/
-sh run.sh configs/vitb_384_DLRSD.yaml 4 output_vitb_384_DLRSD/
-# ####################################################################################
-sh run.sh configs/vitl_336_iSAID.yaml 4 output_vitl_336_iSAID/
-sh run.sh configs/vitb_384_iSAID.yaml 4 output_vitb_384_iSAID/
